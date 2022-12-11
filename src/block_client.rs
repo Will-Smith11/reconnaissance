11909336d047d34e56aebb60226f81c035949da9
Will Smith <williamqsmith11@gmail.com>
2022-12-10 21:54:18 -0500

    add cycler

--- src/block_client.rs
+++ src/block_client.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::sync::{atomic::AtomicUsize, Arc};
 
 use ethers::{
     providers::{JsonRpcClient, Middleware, Provider},
@@ -6,13 +6,35 @@ use ethers::{
 };
 use reth_provider::{BlockProvider, ChainInfo};
 use tokio::{join, runtime::Handle};
+
+/// takes any value and cycles through them
+pub struct Cycler<T>
+{
+    inner: Vec<T>,
+    ptr:   AtomicUsize,
+    max:   usize
+}
+impl<T> Cycler<T>
+{
+    pub fn get(&self) -> Option<&T>
+    {
+        let ptr = if self.ptr.load(std::sync::atomic::Ordering::SeqCst) == self.max
+        {
+            self.ptr.store(0, std::sync::atomic::Ordering::SeqCst);
+            0
+        }
+        else
+        {
+            self.ptr.fetch_add(1, std::sync::atomic::Ordering::SeqCst)
+        };
+        self.inner.get(ptr)
+    }
+}
 /// bit jank because the blockProvider api isn't async
 pub struct BlockClient<P: JsonRpcClient>
 {
     // different clients to delgate calls to
-    inner: Vec<Arc<Provider<P>>>,
-    // a ptr to what client to use, we swap them
-    ptr:   usize
+    inner: Cycler<Arc<Provider<P>>>
 }
 
 impl<P: JsonRpcClient> BlockProvider for BlockClient<P>
@@ -23,17 +45,17 @@ impl<P: JsonRpcClient> BlockProvider for BlockClient<P>
             Handle::current().block_on(async {
                 let latest = self
                     .inner
-                    .get(self.ptr)
+                    .get()
                     .unwrap()
                     .get_block(BlockId::Number(BlockNumber::Latest));
                 let finalized = self
                     .inner
-                    .get(self.ptr)
+                    .get()
                     .unwrap()
                     .get_block(BlockId::Number(BlockNumber::Finalized));
                 let safe = self
                     .inner
-                    .get(self.ptr)
+                    .get()
                     .unwrap()
                     .get_block(BlockId::Number(BlockNumber::Safe));
                 let (latest, finalized, safe) = join!(latest, finalized, safe);
@@ -62,7 +84,27 @@ impl<P: JsonRpcClient> BlockProvider for BlockClient<P>
         id: reth_primitives::rpc::BlockId
     ) -> reth_interfaces::Result<Option<reth_primitives::Block>>
     {
-        todo!()
+        tokio::task::block_in_place(|| {
+            Handle::current().block_on(async {
+                let err = || reth_interfaces::Error::Database(reth_interfaces::db::Error::Read(69));
+                let block = self
+                    .inner
+                    .get()
+                    .unwrap()
+                    .get_block(id)
+                    .await
+                    .map_err(|_| {
+                        reth_interfaces::Error::Database(reth_interfaces::db::Error::Read(69))
+                    })?
+                    .ok_or_else(err)?;
+
+                Ok(Some(reth_primitives::Block {
+                    header: todo!(),
+                    body:   todo!(),
+                    ommers: todo!()
+                }))
+            })
+        })
     }
 
     fn block_number(
@@ -70,7 +112,21 @@ impl<P: JsonRpcClient> BlockProvider for BlockClient<P>
         hash: reth_primitives::H256
     ) -> reth_interfaces::Result<Option<reth_primitives::BlockNumber>>
     {
-        todo!()
+        tokio::task::block_in_place(|| {
+            Handle::current().block_on(async {
+                self.inner
+                    .get()
+                    .unwrap()
+                    .get_block(BlockId::Hash(hash))
+                    .await
+            })
+        })
+        .map(|block| block.map(|block_inner| block_inner.number.unwrap().as_u64()))
+        .map_err(|_| {
+            reth_interfaces::Error::Provider(reth_provider::Error::BlockHashNotExist {
+                block_hash: hash
+            })
+        })
     }
 
     fn block_hash(
@@ -81,7 +137,7 @@ impl<P: JsonRpcClient> BlockProvider for BlockClient<P>
         tokio::task::block_in_place(|| {
             Handle::current().block_on(async {
                 self.inner
-                    .get(self.ptr)
+                    .get()
                     .unwrap()
                     .get_block(BlockId::Number(BlockNumber::Number(number.as_u64().into())))
                     .await
