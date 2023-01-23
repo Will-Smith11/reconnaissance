94869d126f07a3f2226e8563035bc12a9d07a0fa
Will Smith <williamqsmith11@gmail.com>
2023-01-23 11:33:27 -0500

    fix: compatiblity with new reth version

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use reconnaissance::reconnaissance::Reconnaissance;
 #[tokio::main(flavor = "multi_thread")]
 async fn main()
 {
-    reth_tracing::init_tracing();
+    fdlimit::raise_fd_limit();
+    reth_tracing::init_test_tracing();
     let reconnaissance = Reconnaissance::new().await;
     reconnaissance.await
 }
--- src/reconnaissance.rs
+++ src/reconnaissance.rs
@@ -8,6 +8,7 @@ use reth_network::{
     eth_requests::EthRequestHandler, transactions::TransactionsManager, NetworkConfig,
     NetworkHandle, NetworkManager, PeersConfig
 };
+use reth_network_api::PeersInfo;
 use reth_primitives::H512;
 use reth_tracing::tracing::info;
 use reth_transaction_pool::{Pool, PoolConfig};
@@ -77,12 +78,12 @@ impl Reconnaissance
         let disc = SocketAddr::new([0, 0, 0, 0].into(), 30000);
         let reg = SocketAddr::new([0, 0, 0, 0].into(), 30107);
         let secret_key = SecretKey::new(&mut rand::thread_rng());
-        let network_config = NetworkConfig::builder(client.clone(), secret_key)
+        let network_config = NetworkConfig::<Arc<BlockClient<Http>>>::builder(secret_key)
             .boot_nodes(get_boot_nodes())
             .peer_config(peer_config)
             .listener_addr(reg)
             .discovery_addr(disc)
-            .build();
+            .build(client.clone());
 
         let mut network_mng = NetworkManager::new(network_config).await.unwrap();
 
